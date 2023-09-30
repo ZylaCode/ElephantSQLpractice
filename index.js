a07d2f6a654b6590d2f8b51d992f6a7892f3d2e6
ZylaCode <zyla.verena@gmail.com>
2023-09-30 17:03:43 +0200

    with user and order routers

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 import express from 'express';
-import pg from 'pg';
+import userRouter from './routers/userRoutes.js';
+import orderRouter from './routers/orderRoutes.js';
 import 'dotenv/config';
-
+import orderByUserRouter from './routers/orderByUserRouter.js';
 
 const app = express();
-const {Pool} = pg;
 
 app.use(express.json());
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+app.use('/api/orderByUser', orderByUserRouter);
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
 
-app.get("/time", async (req, res) => {
- try{
-    const result = await pool.query('SELECT NOW()');
-    res.json(result.rows[0])
- }catch(err){
-    res.status(500).json(err)
- }
-});
+// app.get("/time", async (req, res) => {
+//  try{
+//     const {rows} = await pool.query('SELECT NOW()');
+//     res.json(rows[0])
+//  }catch(err){
+//     res.status(500).json(err)
+//  }
+// });
+
 
 const port = process.env.PORT || 8000;
 
